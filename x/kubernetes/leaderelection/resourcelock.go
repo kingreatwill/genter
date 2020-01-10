478a23b0902a2025eaef9831d607d5150a68f309
kingreatwill <350840291@qq.com>
2020-01-10 15:06:27 +0800

    k8s

--- x/kubernetes/leaderelection/resourcelock.go
+++ x/kubernetes/leaderelection/resourcelock.go
@@ -2,3 +2,4 @@ package leaderelection
 
 // https://github.com/kubernetes/client-go/blob/master/examples/leader-election/main.go
 // 演示使用领导者选举软件包，该软件包可用于实现HA控制器。
+// 源码:k8s.io/client-go/tools/leaderelection/resourcelock
